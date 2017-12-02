b430fee7df80ba2067f5c50823d5e58c7cf1eb6d
Joshua Smith Laptop <joshner.smith@gmail.com>
2017-12-01 18:14:13 -0800

    - Commit after testing on permissions ready system

--- main.cpp
+++ main.cpp
@@ -206,11 +206,11 @@ int main() {
     pthread_attr_setaffinity_np(&attr4, sizeof(cpu_set_t), &cpu); // Set processor affinity;
 
     // May need to swap these and put them below setschedparam
-    param0.__sched_priority = sched_get_priority_max(SCHED_FIFO); // Max Priority for the system fifo scheduler (99)
+    param0.__sched_priority = sched_get_priority_max(SCHED_FIFO); // Max Priority for the RMS scheduler (99)
     param1.__sched_priority = 90; // 2nd highest priority
-    param2.__sched_priority = 80; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
-    param3.__sched_priority = 70; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
-    param4.__sched_priority = 60; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
+    param2.__sched_priority = 80; // 3rd highest priority
+    param3.__sched_priority = 70; // 4th highest priority
+    param4.__sched_priority = 60; // Lowest priority (it's period is the shortest)
 
     // Set Policy
     pthread_attr_setschedpolicy(&attr0, SCHED_FIFO);
